315e33a889fa2335b857a9e6ae1c530d8badfc09
yunajoe0620 <yunajoe@fineteacher.com>
2025-02-17 10:18:53 +0900

    feat: mp4파일을 mp3파일로 변경가능하게함

--- src/section-one/index.tsx
+++ src/section-one/index.tsx
@@ -11,9 +11,8 @@ import { useEffect, useRef, useState } from "react";
  */
 function SectionOne() {
   const [loaded, setLoaded] = useState(false);
+  const [audioUrl, setAudioUrl] = useState("");
   const ffmpegRef = useRef(new FFmpeg());
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const messageRef = useRef<HTMLParagraphElement | null>(null);
 
   const load = async () => {
     console.log("로드합니다");
@@ -22,22 +21,7 @@ function SectionOne() {
       const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm";
       const ffmpeg = ffmpegRef.current;
       console.log("ffmpeg", ffmpeg);
-      // ffmpeg.on("log", ({ message }) => {
-      //   console.log("message", message);
-      //   if (messageRef.current) messageRef.current.innerHTML = message;
-      // });
 
-      // await ffmpeg.load();
-      // await ffmpeg.load({
-      //   coreURL: await toBlobURL(
-      //     `${baseURL}/ffmpeg-core.js`,
-      //     "text/javascript"
-      //   ),
-      //   wasmURL: await toBlobURL(
-      //     `${baseURL}/ffmpeg-core.wasm`,
-      //     "application/wasm"
-      //   ),
-      // });
       await ffmpeg.load({
         coreURL: await toBlobURL(
           `${baseURL}/ffmpeg-core.js`,
@@ -66,33 +50,35 @@ function SectionOne() {
     console.log("uni8", unit8ArrayData);
     //  writeFile: (path: string, data: FileData, { signal }?: FFMessageOptions) => Promise<OK>;
     //  writefile함슈는 FFmpeg가 사용하는 가상 파일 시스템에 파일을 쓰는 작업
-    // 즉, unit8ArrayData를 라는 데이터를 "/public/test1.avi" 라는 경로에 저장
-    //
-    const result = await ffmpeg.writeFile("/public/test1.avi", unit8ArrayData);
+    // 즉, unit8ArrayData를 라는 데이터를 "test1.mp4" 라는 경로에 저장
+    // 가상 경로에 저장 성공할 경우가 , true 아니면은 false
+    await ffmpeg.writeFile("test1.mp4", unit8ArrayData);
+    // -i test1.mp4 파일은 입력 파일 지정, -q:a 0은 오디오 품질을 0으로 설정, -map a는 오디오만 추출, test1.mp3는 출력 파일 이름 지정
+    // 성공하면은 0, 실패하면은 1
+    await ffmpeg.exec([
+      "-i",
+      "test1.mp4",
+      "-q:a",
+      "0",
+      "-map",
+      "a",
+      "test1.mp3",
+    ]);
 
-    // const result = await ffmpeg.writeFile("/public/input.avi", unit8ArrayData);
-    // const result2 = await ffmpeg.exec(["-i", "input.avi", "output.mp4"]);
-    // console.log("result", result, result2);
-
-    // const videoURL =
-    //   "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/video-15s.avi";
-    // const ffmpeg = ffmpegRef.current;
-    // await ffmpeg.writeFile("input.avi", await fetchFile(filePath));
-    // await ffmpeg.exec(["-i", "input.avi", "output.mp4"]);
-    // const fileData = await ffmpeg.readFile("output.mp4");
-    // const data = new Uint8Array(fileData as ArrayBuffer);
-    // console.log("data", data);
-    // if (videoRef.current) {
-    //   videoRef.current.src = URL.createObjectURL(
-    //     new Blob([data.buffer], { type: "video/mp4" })
-    //   );
-    // }
+    // result는 unit8Array
+    const data = await ffmpeg.readFile("test1.mp3");
+    // Blob {size: 53868242, type: 'audio/mp3'}
+    const audioBlob = new Blob([data], { type: "audio/mp3" });
+    const audioUrl = URL.createObjectURL(audioBlob);
+    setAudioUrl(audioUrl);
+    setLoaded(false);
   };
 
   useEffect(() => {
     load();
   }, []);
   console.log("load", loaded);
+  console.log("audioUrl");
 
   return (
     <>
