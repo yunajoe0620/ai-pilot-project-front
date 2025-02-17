4384bd2d1d8ffd593b738d06f13b411962fcaba1
yunajoe0620 <yunajoe@fineteacher.com>
2025-02-17 17:33:01 +0900

    feat: section 2만들기

--- src/section-two/index.tsx
+++ src/section-two/index.tsx
@@ -13,13 +13,40 @@ function SectionTwo() {
   const [problemType, setProblemType] = useState();
   return (
     <div>
-      <h1>맞춤형 학습</h1>
+      <h1 className="font-bold">맞춤형 학습</h1>
       <div>
-        <input />
-        <input />
-        <input />
-        <input />
-        <input />
+        {/* 대상 선택하기 */}
+        <div>
+          <select>
+            <option disabled selected>
+              대상 선택하기
+            </option>
+            <option value="elementary">초등학교</option>
+            <option value="middle">중학교</option>
+            <option value="highschool">고등학교</option>
+            <option value="university">대학교</option>
+          </select>
+        </div>
+        {/* 과목선택 */}
+        <div>
+          <label>과목</label>
+          <input value={subject} />
+        </div>
+        {/* 주제선택 */}
+        <div>
+          <label>주제</label>
+          <input value={theme} />
+        </div>
+        {/* 난이도 선택 */}
+        <div>
+          <label>난이도</label>
+          <input value={level} />
+        </div>
+        {/* 문제유형 선택 */}
+        <div>
+          <label>문제유형</label>
+          <input value={problemType} />
+        </div>
       </div>
     </div>
   );
