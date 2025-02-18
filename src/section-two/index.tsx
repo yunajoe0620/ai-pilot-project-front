9226b0e478ac3681f732daa2b17fa4bdd3437b03
yunajoe0620 <yunajoe@fineteacher.com>
2025-02-18 13:53:07 +0900

    feat: 폴더구조 만드는 중

--- src/section-two/index.tsx
+++ src/section-two/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { baseUrl } from "../api";
 
 function SectionTwo() {
   // 대상
@@ -10,7 +11,7 @@ function SectionTwo() {
   //   난이도
   const [level, setLevel] = useState("난이도선택하기");
   //   문제유형
-  const [problemType, setProblemType] = useState();
+  const [problemType, setProblemType] = useState("문제유형선택하기");
 
   const handleTarget = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTarget(e.target.value);
@@ -28,59 +29,82 @@ function SectionTwo() {
     setLevel(e.target.value);
   };
 
-  console.log("ggg");
+  const handleProblemType = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setProblemType(e.target.value);
+  };
+
+  const handleGenerateProblems = async () => {
+    console.log("문제 생성합니다");
+    console.log("결과값", target, subject, theme, level, problemType);
+    const result = await fetch(`${baseUrl}/problem/generate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({ target, subject, theme, level, problemType }),
+    });
+  };
 
   return (
-    <div className="h-full w-full flex flex-col items-center bg-gray-100">
-      <h1 className="font-bold">AI 맞춤형 학습</h1>
-      <div>
-        {/* 대상 선택하기 */}
-        <div className="flex flex-row gap-16">
-          <label>학생 학년 및 선택 수준</label>
-          <select className="border-2" onChange={handleTarget}>
-            <option value="text-menu">대상선택하기</option>
-            <option value="elementary">초등학교</option>
-            <option value="middle">중학교</option>
-            <option value="highschool">고등학교</option>
-            <option value="university">대학교</option>
-          </select>
-        </div>
-        {/* 과목선택 */}
-        <div className="flex flex-row gap-16">
-          <label>과목</label>
-          <select className="border-2" onChange={handleSubject}>
-            <option value="text-menu">과목 선택하기</option>
-            <option value="math">수학</option>
-            <option value="english">영어</option>
-            <option value="science">과학</option>
-            <option value="history">역사</option>
-          </select>
-        </div>
-        {/* 주제선택 */}
-        <div className="flex flex-row gap-16">
-          <label>주제</label>
-          <input className="border-2" onChange={handleTheme} value={theme} />
-        </div>
-        {/* 난이도 선택 */}
-        <div className="flex flex-row gap-16">
-          <label>난이도</label>
-          <select className="border-2" onChange={handleLevel}>
-            <option value="text-menu">난이도 선택하기</option>
-            <option value="easy">쉬움</option>
-            <option value="medium">보통</option>
-            <option value="difficulty">어려움</option>
-          </select>
-        </div>
-        {/* 문제유형 선택 */}
-        <div className="flex flex-row gap-16">
-          <label>문제유형</label>
-          <select className="border-2">
-            <option value="text-menu">문제 유형 선택하기</option>
-            <option value="easy">객관식</option>
-            <option value="medium">주관식</option>
-          </select>
+    <div className="flex flex-col items-center">
+      <div className="h-full w-full flex flex-col items-center bg-gray-100">
+        <h1 className="font-bold">AI 맞춤형 학습</h1>
+        <div>
+          {/* 대상 선택하기 */}
+          <div className="flex flex-row gap-16">
+            <label>학생 학년 및 선택 수준</label>
+            <select className="border-2" onChange={handleTarget}>
+              <option value="text-menu">대상선택하기</option>
+              <option value="elementary">초등학교</option>
+              <option value="middle">중학교</option>
+              <option value="highschool">고등학교</option>
+              <option value="university">대학교</option>
+            </select>
+          </div>
+          {/* 과목선택 */}
+          <div className="flex flex-row gap-16">
+            <label>과목</label>
+            <select className="border-2" onChange={handleSubject}>
+              <option value="text-menu">과목 선택하기</option>
+              <option value="math">수학</option>
+              <option value="english">영어</option>
+              <option value="science">과학</option>
+              <option value="history">역사</option>
+            </select>
+          </div>
+          {/* 주제선택 */}
+          <div className="flex flex-row gap-16">
+            <label>주제</label>
+            <input className="border-2" onChange={handleTheme} value={theme} />
+          </div>
+          {/* 난이도 선택 */}
+          <div className="flex flex-row gap-16">
+            <label>난이도</label>
+            <select className="border-2" onChange={handleLevel}>
+              <option value="text-menu">난이도 선택하기</option>
+              <option value="easy">쉬움</option>
+              <option value="medium">보통</option>
+              <option value="difficulty">어려움</option>
+            </select>
+          </div>
+          {/* 문제유형 선택 */}
+          <div className="flex flex-row gap-16">
+            <label>문제유형</label>
+            <select className="border-2" onChange={handleProblemType}>
+              <option value="text-menu">문제 유형 선택하기</option>
+              <option value="short-answer-question">객관식</option>
+              <option value="essay-question">주관식</option>
+            </select>
+          </div>
         </div>
       </div>
+      <button
+        className="border-2 bg-gray-700 text-amber-100 w-[200px] p-4"
+        onClick={handleGenerateProblems}
+      >
+        문제 생성 하기
+      </button>
     </div>
   );
 }
