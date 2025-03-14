fecdccba40b7efa852c58ecd2ea30f3660d48b20
yunajoe0620 <yunajoe@fineteacher.com>
2025-03-14 09:37:35 +0900

    feat: url변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
 
-// export const baseUrl = "http://192.168.0.20:5000";
+export const baseUrl = "http://192.168.0.20:5000";
